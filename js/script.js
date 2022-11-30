8ee6af7eaf9ee1cce4eadf1f83bf9ae872aa9328
Pavlo Rozhko <91882494+PavloRozhko@users.noreply.github.com>
2022-11-30 18:41:14 +0200

    new

---
+++ js/script.js
@@ -0,0 +1,11 @@
+"use strict"
+// Синтаксис
+console.log(`Я вчу JS!`);
+//Змінні
+let userEyesColor; // Назва змінної для зберігання крльрпу очей користувача
+
+let user;
+let userName;
+userName = `Вася`;
+user = userName;
+console.log(user);
